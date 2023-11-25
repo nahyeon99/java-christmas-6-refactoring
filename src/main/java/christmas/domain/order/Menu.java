d91166f747f9a736daeead28d3c7c06589fb1767
nahyeon99 <nahyeonee99@gmail.com>
2023-11-25 19:00:00 +0900

    feat(order/Menu): 메뉴 Enum 클래스 추가
    - 모든 메뉴는 Menu.class(Enum)으로 관리한다.
    - 메뉴의 카테고리 분류를 위한 MenuCategory.class(Enum)을 둔다. 이는 Menu에서 필드로 관리한다.
    - 사용자의 메뉴 입력을 인수로 받아 Enum 클래스 내 일치하는 Enum을 반환한다.

---
+++ src/main/java/christmas/domain/order/Menu.java
@@ -0,0 +1,46 @@
+package christmas.domain.order;
+
+import static christmas.exception.ExceptionMessage.INVALID_ORDER;
+
+import java.util.Arrays;
+
+public enum Menu {
+    MUSHROOM_SOUP(MenuCategory.APPETIZER, 6_000, "양송이수프"),
+    TAPAS(MenuCategory.APPETIZER, 5_500, "타파스"),
+    CAESAR_SALAD(MenuCategory.APPETIZER, 8_000, "시저샐러드"),
+
+    T_BONE_STEAK(MenuCategory.MAIN, 55_000, "티본스테이크"),
+    BBQ_RIBS(MenuCategory.MAIN, 54_000, "바비큐립"),
+    SEAFOOD_PASTA(MenuCategory.MAIN, 35_000, "해산물파스타"),
+    CHRISTMAS_PASTA(MenuCategory.MAIN, 25_000, "크리스마스파스타"),
+
+    CHOCOLATE_CAKE(MenuCategory.DESSERT, 15_000, "초코케이크"),
+    ICE_CREAM(MenuCategory.DESSERT, 5_000, "아이스크림"),
+
+    ZERO_COLA(MenuCategory.BEVERAGE, 3_000, "제로콜라"),
+    RED_WINE(MenuCategory.BEVERAGE, 60_000, "레드와인"),
+    CHAMPAGNE(MenuCategory.BEVERAGE, 25_000, "샴페인"),
+
+    NONE(MenuCategory.NONE, 0, "없음");
+
+    private final MenuCategory category;
+    private final int price;
+    private final String korean;
+
+    Menu(MenuCategory menuCategory, int price, String korean) {
+        this.category = menuCategory;
+        this.price = price;
+        this.korean = korean;
+    }
+
+    public static Menu from(String menu) {
+        return Arrays.stream(values())
+                .filter(item -> item.getKorean().equals(menu.replaceAll(" ", "")))
+                .findAny()
+                .orElseThrow(() -> new IllegalStateException(INVALID_ORDER.getMessage()));
+    }
+
+    public String getKorean() {
+        return korean;
+    }
+}
---
+++ src/main/java/christmas/domain/order/MenuCategory.java
@@ -0,0 +1,9 @@
+package christmas.domain.order;
+
+public enum MenuCategory {
+    APPETIZER,
+    MAIN,
+    DESSERT,
+    BEVERAGE,
+    NONE;
+}
