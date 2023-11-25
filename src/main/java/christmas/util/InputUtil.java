5341ef0e8eef81a4521a9770151b009bff27f1cd
nahyeon99 <nahyeonee99@gmail.com>
2023-11-25 19:17:18 +0900

    refactor(util/InputUtil): 메서드 시그니처 변경

--- src/main/java/christmas/util/InputUtil.java
+++ src/main/java/christmas/util/InputUtil.java
@@ -4,40 +4,13 @@ import static christmas.exception.ExceptionMessage.INVALID_BLANK_INPUT;
 import static christmas.exception.ExceptionMessage.INVALID_NUMBER_FORMAT;
 
 import camp.nextstep.edu.missionutils.Console;
-import christmas.view.ConsoleOutputView;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.function.Supplier;
 
 public class InputUtil {
-    private static final String MENU_SEPERATOR = ",";
-
-    public static int readNumber() {
-        while (true) {
-            try {
-                return Integer.parseInt(readLine());
-            } catch (NumberFormatException exception) {
-                throw new IllegalArgumentException(INVALID_NUMBER_FORMAT.getMessage());
-            }
-        }
-    }
-
-    public static List<String> readList() {
-        return seperate(readLine());
-    }
-
-    public static <T> T repeatUntilValidInput(String errorMessage, Supplier<T> repeatSupplier) {
-        while (true) {
-            try {
-                return repeatSupplier.get();
-            } catch (RuntimeException exception) {
-                ConsoleOutputView.printExceptionMessage(exception.getMessage());
-            }
-        }
-    }
-
-    private static String readLine() {
+    public static String readLine() {
         while (true) {
             String input = Console.readLine();
             if (Objects.isNull(input) || input.isBlank()) {
@@ -46,8 +19,32 @@ public class InputUtil {
         }
     }
 
-    private static List<String> seperate(final String input) {
-        return Arrays.stream(input.split(MENU_SEPERATOR))
+    public static int readNumber(String input) {
+        while (true) {
+            try {
+                return Integer.parseInt(input);
+            } catch (NumberFormatException exception) {
+                throw new IllegalArgumentException(INVALID_NUMBER_FORMAT.getMessage());
+            }
+        }
+    }
+
+    public static List<String> readList(String input, String delimiter) {
+        return seperate(input, delimiter);
+    }
+
+    public static <T> T repeatUntilValidInput(String errorMessage, Supplier<T> repeatSupplier) {
+        while (true) {
+            try {
+                return repeatSupplier.get();
+            } catch (RuntimeException exception) {
+                System.out.println(exception.getMessage());
+            }
+        }
+    }
+
+    private static List<String> seperate(final String input, final String delimiter) {
+        return Arrays.stream(input.split(delimiter))
                 .toList();
     }
 }
